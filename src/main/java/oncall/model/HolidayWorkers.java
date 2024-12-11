d8dbf81f095d93a59884276e1529f14d1ed46b2d
양효빈 <hyobin3026@naver.com>
2024-12-11 21:27:16 +0900

    feat: 평일, 휴일 근무자 모델 구현

---
+++ src/main/java/oncall/model/HolidayWorkers.java
@@ -0,0 +1,31 @@
+package oncall.model;
+
+import java.util.List;
+
+public class HolidayWorkers {
+
+    private List<String> workers;
+
+    public HolidayWorkers(List<String> workers){
+        this.workers = workers;
+    }
+
+    public String getNameByIndex(int index){
+        return workers.get(index);
+    }
+
+    public void changeWorkerToNext(int index){
+        String temp = workers.get(index);
+        if ((index+1) >= workers.size()){
+            workers.set(index, workers.get(0));
+            workers.set(0, temp);
+            return;
+        }
+        workers.set(index, workers.get(index+1));
+        workers.set(index+1, temp);
+    }
+
+    public boolean isSameAsWorkersSize(int number){
+        return number == workers.size();
+    }
+}
